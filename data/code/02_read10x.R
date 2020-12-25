82c82a532cfc715f8f8422771662b5746a494089
Wenpin Hou <wp.hou3@gmail.com>
2020-12-25 10:11:10 -0500

    add process myeloid

---
+++ data/code/02_read10x.R
@@ -0,0 +1,10 @@
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/raisin/proc/read10x.R')
+read10x(list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/Tumor/',full.names = T),'/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/proc/tumor/all',list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/Tumor/'),verbose = T)
+
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/raisin/proc/read10x.R')
+read10x(list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/myeloid/',full.names = T),'/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/proc/myeloid',list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/myeloid'),verbose = T)
+
+
+
+
+
