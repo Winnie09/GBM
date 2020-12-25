ed8e762942ea9f76453a0902a4b3a0b533c733dd
Wenpin Hou <wp.hou3@gmail.com>
2020-12-25 11:49:28 -0500

    add lymph

--- data/code/02_read10x.R
+++ data/code/02_read10x.R
@@ -4,7 +4,5 @@ read10x(list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/Tumo
 source('/home-4/whou10@jhu.edu/scratch/Wenpin/raisin/proc/read10x.R')
 read10x(list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/myeloid/',full.names = T),'/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/proc/myeloid',list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/myeloid'),verbose = T)
 
-
-
-
-
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/raisin/proc/read10x.R')
+read10x(list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/lymph/',full.names = T),'/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/proc/lymph',list.files('/home-4/whou10@jhu.edu/work-zfs/whou10/GBM/data/raw/dge/lymph'),verbose = T)
